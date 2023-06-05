c53ee3134c6bf64a3fff275a6fcd9123eb1a9434
Gandhemalle Navya sri <93768093+Navya53@users.noreply.github.com>
2023-06-05 22:46:54 +0530

    js

--- usercard2.js
+++ usercard2.js
@@ -1,12 +1,12 @@
 let Viratinfo={
     imgurl: "//www.suntiros.com/wp-content/uploads/2017/09/Virat-Kohli-HD-Photos-Gallery.jpg",
     name: "Virat Kohli",
-    Description: "Indian Cricketer"
+    description: "Indian Cricketer"
 }
 let Viratyounginfo={
     imgurl: "http://1.bp.blogspot.com/-iV6xrJt2vpY/UaB6BMlYp6I/AAAAAAAAJQI/IbngTaQmDhM/s1600/cricketer-virat-kohli-childhood-pictures-childhood-images.blogspot.com-(2).jpg",
     name: "Virat Kohli",
-    Description: "Indian Cricketer"
+    description: "Indian Cricketer"
 }
 let isViratinfo=true;
 let displayobject;
@@ -19,6 +19,6 @@ let flipdata=function(){
         displayobject=Viratinfo;
         isViratinfo=true;
     }
-    document.getElemnentById("Virat-img").src=displayobject.imgurl;
-    document.getElemnentById("Virat-name").innerHTML=displayobject.name;
-}   document.getElemnentById("Virat-description").innerHTML=displayobject.Description;
+    document.getElemnentById("Virat-img").src = displayobject.imgurl;
+    document.getElemnentById("Virat-name").innerHTML = displayobject.name;
+}   document.getElemnentById("Virat-description").innerHTML = displayobject.description;
