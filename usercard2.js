73703b61e52039396e6ed652e3597104c9b5eaa9
Gandhemalle Navya sri <93768093+Navya53@users.noreply.github.com>
2023-06-05 22:29:24 +0530

    js

--- usercard2.js
+++ usercard2.js
@@ -3,3 +3,22 @@ let Viratinfo{
     name: "Virat Kohli",
     Description: Indian Cricketer
 }
+let Viratyounginfo{
+    imgurl: "http://1.bp.blogspot.com/-iV6xrJt2vpY/UaB6BMlYp6I/AAAAAAAAJQI/IbngTaQmDhM/s1600/cricketer-virat-kohli-childhood-pictures-childhood-images.blogspot.com-(2).jpg",
+    name: "Virat Kohli",
+    Description: Indian Cricketer
+}
+let isViratinfo=true;
+let displayobject;
+let flipdata=function(){
+    if(isViratinfo==true){
+        displayobject=Viratyounginfo;
+        isViratinfo=false;
+    }
+    else{
+        displayobject=Viratinfo;
+        isViratinfo=true;
+    }
+    document.getElemnentById("Virat-img").src=displayobject.imgurl;
+    document.getElemnentById("Virat-name").innerHTML=displayobject.name;
+}   document.getElemnentById("Virat-description").innerHTML=displayobject.Descripti;
