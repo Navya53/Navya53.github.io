36d3ee74fbee5ac00155a80f3b48179a4210696c
Gandhemalle Navya sri <93768093+Navya53@users.noreply.github.com>
2023-06-07 17:38:43 +0530

    usercard

--- usercard2.js
+++ usercard2.js
@@ -1,25 +1,27 @@
-let Viratinfo={
+let Viratinfo = {
     imgurl: "//www.suntiros.com/wp-content/uploads/2017/09/Virat-Kohli-HD-Photos-Gallery.jpg",
     name: "Virat Kohli",
     description: "Indian Cricketer"
-}
-let Viratyounginfo={
+  };
+  
+  let Viratyounginfo = {
     imgurl: "https://st1.photogallery.ind.sh/wp-content/uploads/indiacom/virat-kohli-in-his-childhood-201605-1464674807.jpg",
     name: "Virat Kohli",
     description: "Indian Cricketer"
-}
-let isViratinfo=true;
-let displayobject;
-let flipdata=function(){
-    if(isViratinfo==true){
-        displayobject=Viratyounginfo;
-        isViratinfo=false;
+  };
+  
+  let isViratinfo = true;
+  let displayobject;
+  
+  let flipdata = function() {
+    if (isViratinfo == true) {
+      displayobject = Viratyounginfo;
+      isViratinfo = false;
+    } else {
+      displayobject = Viratinfo;
+      isViratinfo = true;
     }
-    else{
-        displayobject=Viratinfo;
-        isViratinfo=true;
-    }
-    document.getElemnentById("Virat-img").src = displayobject.imgurl;
-    document.getElemnentById("Virat-name").innerHTML = displayobject.name;
-    document.getElemnentById("Virat-description").innerHTML = displayobject.description;
-}
+    document.getElementById("Virat-img").src = displayobject.imgurl;
+    document.getElementById("Virat-name").innerHTML = displayobject.name;
+    document.getElementById("Virat-description").innerHTML = displayobject.description;
+  };
