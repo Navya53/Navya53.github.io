16c3fc1549bb90ef584fab1e5b1517f4079de4fc
Gandhemalle Navya sri <93768093+Navya53@users.noreply.github.com>
2023-06-13 22:24:13 +0530

    changes done

--- usercard3.js
+++ usercard3.js
@@ -12,9 +12,10 @@ let Viratinfo = {
     fetch("https://randomuser.me/api")
     .then(response => response.json())
     .then(data=>{
-        displayobject.name=data.results[0].name.first + " " + data.results[0].name.last
-        displayobject.imageurl=data.results[0].picture.large
-        displayobject.description=data.results[0].gender
+        //displayobject.name=data.results[0].name.first + " " + data.results[0].name.last
+        displayobject.name="User Name"
+        //displayobject.imageurl=data.results[0].picture.large
+        //displayobject.description=data.results[0].gender
         document.getElementById("Virat-img").src = displayobject.imgurl;
         document.getElementById("Virat-name").innerHTML = displayobject.name;
         document.getElementById("Virat-description").innerHTML = displayobject.description;
